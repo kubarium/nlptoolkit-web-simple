04e9df2796cbb12e7f066e99d063262cbc3e3ff1
olcaytaner <olcaytaner@gmail.com>
2024-10-07 10:12:07 +0300

    Added pos tagger.

---
+++ js/english-pos-tag.js
@@ -0,0 +1,36 @@
+function include(file) {
+    let script = document.createElement('script');
+    script.src = file;
+    script.type = 'text/javascript';
+    script.defer = true;
+    document.getElementsByTagName('head').item(0).appendChild(script);
+}
+
+include('../data/english-tags.js');
+
+document.getElementById('posTag').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const sentence = document.getElementById('sentence').value;
+    let words = sentence.toLowerCase().split(' ');
+    let display = "<table> <tr> <th>Word</th> <th>Tag(s)</th> </tr>";
+    for (let word of words) {
+        display = display + "<tr><td>" + word + "</td>"
+        let found = false
+        for (let i = 0; i < englishTags.length; i++) {
+            const wordObject = englishTags[i];
+            const wordName = wordObject["word"]
+            const wordTags = wordObject["tagSet"]
+            if (word === wordName){
+                found = true
+                display = display + "<td>" + wordTags + "</td>"
+                break;
+            }
+        }
+        if (!found){
+            display = display + "<td>UNK</td>"
+        }
+        display = display + "</tr>"
+    }
+    display = display + "</table>"
+    document.getElementById("result").innerHTML = display;
+})
