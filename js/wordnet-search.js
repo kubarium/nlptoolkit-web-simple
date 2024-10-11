bc89a1b3535ebee4d715cd4e0ac0994183f46e57
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2024-10-11 10:36:23 +0300

    Added binary search for ids and linear search for words.

--- js/wordnet-search.js
+++ js/wordnet-search.js
@@ -1,4 +1,4 @@
-function createSynonym(display, j, words){
+function createSynonym(display, j, words) {
     let t = 0
     for (let k = 0; k < words.length; k++) {
         if (k !== j) {
@@ -14,10 +14,41 @@ function createSynonym(display, j, words){
     return display
 }
 
-function createTableForWordSearch(word, wordNet){
-    let display = "<table> <tr> <th>Id</th> <th>Pos</th> <th>Definition</th> <th>Synonyms</th> </tr>";
+function getSynsetsWithWord(word, wordNet) {
+    let result = []
     for (let i = 0; i < wordNet.length; i++) {
         let synset = wordNet[i];
+        for (let j = 0; j < synset["words"].length; j++) {
+            if (synset["words"][j] === word) {
+                result.push(synset)
+                break;
+            }
+        }
+    }
+    return result
+}
+
+function getSynsetWithId(id, wordNet){
+    let low = 0, high = wordNet.length - 1;
+    while (low < high) {
+        let middle = Math.floor((low + high) / 2)
+        if (wordNet[middle]["id"] === id){
+            return wordNet[middle]
+        } else {
+            if (wordNet[middle]["id"] < id){
+                low = middle + 1
+            } else {
+                high = middle - 1
+            }
+        }
+    }
+    return null
+}
+
+function createTableForWordSearch(word, wordNet) {
+    let display = "<table> <tr> <th>Id</th> <th>Pos</th> <th>Definition</th> <th>Synonyms</th> </tr>";
+    let wordList = getSynsetsWithWord(word, wordNet)
+    for (let synset of wordList) {
         for (let j = 0; j < synset["words"].length; j++) {
             if (synset["words"][j] === word) {
                 display = display + "<tr><td>" + synset["id"] + "</td><td>" + synset["pos"] + "</td><td>" + synset["definition"] + "</td><td>"
@@ -30,15 +61,17 @@ function createTableForWordSearch(word, wordNet){
     return display
 }
 
-function createTableForSynonymSearch(synonymWord, wordNet){
+function createTableForSynonymSearch(synonymWord, wordNet) {
     let display = "<table> <tr> <th>Synonym Words</th></tr>";
-    for (let i = 0; i < wordNet.length; i++) {
-        let synset = wordNet[i];
-        for (let j = 0; j < synset["words"].length; j++) {
-            if (synset["words"][j] === synonymWord && synset["words"].length !== 1) {
-                display = display + "<tr><td>";
-                display = createSynonym(display, j, synset["words"])
-                break;
+    let wordList = getSynsetsWithWord(synonymWord, wordNet);
+    for (let synset of wordList) {
+        if (synset["words"].length !== 1) {
+            for (let j = 0; j < synset["words"].length; j++) {
+                if (synset["words"][j] === synonymWord) {
+                    display = display + "<tr><td>";
+                    display = createSynonym(display, j, synset["words"])
+                    break;
+                }
             }
         }
     }
@@ -46,16 +79,13 @@ function createTableForSynonymSearch(synonymWord, wordNet){
     return display
 }
 
-function createTableForIdSearch(synsetId, wordNet){
+function createTableForIdSearch(synsetId, wordNet) {
     let display = "<table> <tr> <th>Pos</th> <th>Definition</th> <th>Synonyms</th> </tr>";
-    for (let i = 0; i < wordNet.length; i++) {
-        let synset = wordNet[i];
-        if (synset["id"] === synsetId) {
-            display = display + "<tr><td>" + synset["pos"] + "</td><td>" + synset["definition"] + "</td><td>";
-            display = createSynonym(display, -1, synset["words"])
-            break;
-        }
+    let synset = getSynsetWithId(synsetId, wordNet);
+    if (synset != null){
+        display = display + "<tr><td>" + synset["pos"] + "</td><td>" + synset["definition"] + "</td><td>";
+        display = createSynonym(display, -1, synset["words"])
+        display = display + "</table>"
     }
-    display = display + "</table>"
     return display
 }
