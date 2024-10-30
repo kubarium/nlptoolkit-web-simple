0b73f2b73678eabbb8f3d7776c8de189f4eca05c
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2024-10-30 15:23:36 +0300

    Added word sense disambiguation.

---
+++ js/turkish/word-sense-disambiguation.js
@@ -0,0 +1,16 @@
+function include(file) {
+    let script = document.createElement('script');
+    script.src = file;
+    script.type = 'text/javascript';
+    script.defer = true;
+    document.getElementsByTagName('head').item(0).appendChild(script);
+}
+
+include('../data/turkish/turkish-word-sense-disambiguation.js');
+include('../js/word-table.js');
+
+document.getElementById('wordSenseDisambiguation').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const sentence = document.getElementById('sentence').value;
+    document.getElementById("result").innerHTML = createWordTableForSentence(sentence, "Word Sense Disambiguation", wordSenseDisambiguation, "sense", true, false);
+})
