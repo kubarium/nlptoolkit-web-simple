2154261122c12c6aad22ec2600ef775085b015f8
Kumsal Obuz <iam@denizkumsal.com>
2025-02-08 12:10:41 -0500

    Marks the active page in the navigation

--- js/website/nlp_toolkit.js
+++ js/website/nlp_toolkit.js
@@ -5,11 +5,17 @@ $(function () {
     $(".uk-navbar-dropdown-nav a").on("click", (event) => {
         event.preventDefault();
 
+        //open the requested page in the iframe
         $("iframe").attr("src", $(event.target).attr("href"));
 
+        //update the breadcrumb
         $("#language").text($(event.target).parents(".uk-navbar-dropdown").siblings("a").text().toString());
-        $("#toolkit").text($(event.target).text())
-        $(".uk-breadcrumb").show()
+        $("#toolkit").text($(event.target).text());
+        $(".uk-breadcrumb").show();
+
+        //mark the active page in the navigation
+        $(event.target).parent().siblings("li").removeClass("uk-active");
+        $(event.target).parent().addClass("uk-active");
     })
 
     $(".uk-navbar-dropdown-nav a").first().trigger("click")
