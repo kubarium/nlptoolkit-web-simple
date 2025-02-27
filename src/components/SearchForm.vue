e9109e268dff82c39a8e43001f6d1a2500d932aa
Kumsal Obuz <iam@denizkumsal.com>
2025-02-26 20:32:43 -0500

    Constructs a search form component

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,13 @@
+<script setup>
+const model = defineModel()
+</script>
+
+<template>
+  <form>
+    <fieldset class="uk-flex">
+      <legend><slot></slot></legend>
+      <input class="uk-input" type="text" required v-model="model" />
+      <button class="uk-button uk-button-primary" type="submit">Find</button>
+    </fieldset>
+  </form>
+</template>
