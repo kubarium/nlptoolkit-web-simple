bc2acac943991716ca965a7ec65b7c839bc171f2
Kumsal Obuz <iam@denizkumsal.com>
2025-03-08 16:28:11 -0500

    Moves NLP functions to their own library files + UX

--- src/views/TurkishFramenet.vue
+++ src/views/TurkishFramenet.vue
@@ -1,20 +1,22 @@
 <script setup>
 import SearchForm from "@/components/SearchForm.vue"
+import SearchResultsCounter from "@/components/SearchResultsCounter.vue"
 import turkishFrameNet from "@/data/turkish/turkish-framenet.json"
 import turkishWordNet from "@/data/turkish/turkish-wordnet.json"
 import { reactive, ref } from "vue"
+import * as utils from "@/utils/nlp"
+import { scrollToTop } from "@/utils/scrollToTop"
 
-defineOptions({ metaInfo: { language: "Turkish", toolkit: "FrameNet" } })
+defineOptions({ meta: { language: "Turkish", toolkit: "FrameNet" } })
 
 const searchTerms = reactive({
   frame: "", verb: "", id: ""
 })
-const searching = ref("")
+const search = ref("")
 const searchResults = ref([])
 
-
 function findFrame(frame) {
-  searching.value = frame
+  search.value = frame
   searchTerms.frame = ""
   const entry = turkishFrameNet.find(entry => entry.frame == frame)
   searchResults.value = [{
@@ -25,72 +27,18 @@ function findFrame(frame) {
   scrollToTop()
 }
 function findVerb(word) {
-  searching.value = word
+  search.value = word
   searchTerms.verb = ""
-  searchResults.value = getFramesForSynSets(getSynsetsWithWord(word, turkishWordNet))
+  searchResults.value = utils.getFramesForSynSets(utils.getSynsetsWithWord(word, turkishWordNet), turkishFrameNet)
   scrollToTop()
 }
 function findSynSetID(word) {
-  searching.value = word
+  search.value = word
   searchTerms.id = ""
-  searchResults.value = getFramesForSynSet(word)
+  searchResults.value = utils.getFramesForSynSet(word, turkishFrameNet)
   scrollToTop()
 }
 
-function scrollToTop() {
-  window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
-}
-
-function getSynsetsWithWord(word, wordNet) {
-  let result = []
-  for (let i = 0; i < wordNet.length; i++) {
-    let synset = wordNet[i];
-    for (let j = 0; j < synset["words"].length; j++) {
-      if (synset["words"][j] === word) {
-        result.push(synset)
-        break;
-      }
-    }
-  }
-  return result
-}
-function searchInWordnet(id, wordNet) {
-  return wordNet.find(entry => entry.id == id) || []
-}
-function getFramesForSynSet(synset) {
-  let result = []
-  for (let i = 0; i < turkishFrameNet.length; i++) {
-    let frame = turkishFrameNet[i];
-    let lexicalUnits = frame["lexicalUnits"];
-    for (let lexicalUnit of lexicalUnits) {
-      if (lexicalUnit === synset) {
-        result.push(frame)
-        break;
-      }
-    }
-  }
-  return result
-}
-function frameListContains(frames, frame) {
-  for (let current of frames) {
-    if (current["frame"] === frame["frame"]) {
-      return true
-    }
-  }
-  return false
-}
-function getFramesForSynSets(synsets) {
-  let result = []
-  for (let synset of synsets) {
-    let current = getFramesForSynSet(synset["id"]);
-    for (let frame of current) {
-      if (!frameListContains(result, frame)) {
-        result.push(frame)
-      }
-    }
-  }
-  return result
-}
 </script>
 
 <template>
@@ -99,12 +47,8 @@ function getFramesForSynSets(synsets) {
     <SearchForm @submit.prevent="findVerb(searchTerms.verb)" v-model="searchTerms.verb">Verb</SearchForm>
     <SearchForm @submit.prevent="findSynSetID(searchTerms.id)" v-model="searchTerms.id">Verb SynSet Id</SearchForm>
   </header>
-  <template v-if="searching">
-    <h2 class="uk-h3 uk-text-muted uk-text-center">
-      {{ searchResults.length }} result{{ searchResults.length > 1 ? 's' : searchResults.length == 0 ? 's' : '' }}
-      for {{ searching }}
-    </h2>
-    <hr class="uk-divider-icon" />
+  <template v-if="search">
+    <SearchResultsCounter :searchResults="searchResults" :for="search" />
   </template>
   <ul uk-accordion class="framenet-verb">
     <li v-for="(result, index) in searchResults" :key="index">
@@ -117,7 +61,7 @@ function getFramesForSynSets(synsets) {
         <div uk-grid>
           <div class="uk-width-2-3">
             <span class="uk-label uk-margin-xsmall-right" v-for="frame in result.frameElements" :key="frame">{{ frame
-            }}</span>
+              }}</span>
           </div>
           <div class="uk-width-1-3 uk-divider-vertical">
             <button class="uk-button uk-button-secondary uk-width-1-1 uk-height-1-1"
@@ -142,12 +86,12 @@ function getFramesForSynSets(synsets) {
             <tr v-for="unit in result.lexicalUnits" :key="unit">
               <td> <a @click="findSynSetID(unit)"> {{ unit }} </a> </td>
               <td>
-                <template v-for="(word, index) in searchInWordnet(unit, turkishWordNet)['words']" :key="word">
+                <template v-for="(word, index) in utils.searchInWordnet(unit, turkishWordNet)['words']" :key="word">
                   <span v-if="index > 0">; </span>
                   <a @click="findVerb(word)">{{ word }}</a>
                 </template>
               </td>
-              <td> {{ searchInWordnet(unit, turkishWordNet)["definition"] }} </td>
+              <td> {{ utils.searchInWordnet(unit, turkishWordNet)["definition"] }} </td>
             </tr>
           </tbody>
         </table>
