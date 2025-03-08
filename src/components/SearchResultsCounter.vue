6439591aa92e77bdfca3c94a615a22044aa709cc
Kumsal Obuz <iam@denizkumsal.com>
2025-03-08 16:03:02 -0500

    Extracts search results into its component

---
+++ src/components/SearchResultsCounter.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+  searchResults: Array,
+  for: String
+})
+</script>
+
+<template>
+  <h2 class="uk-h3 uk-text-muted uk-text-center">
+    {{ props.searchResults.length }} result{{ props.searchResults.length > 1 ? 's' : props.searchResults.length == 0 ?
+      's' : '' }}
+    for {{ props.for }}
+  </h2>
+  <hr class="uk-divider-icon" />
+</template>
