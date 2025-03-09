8b37c6071d260e5a8e0a905823756c8802404afe
Kumsal Obuz <iam@denizkumsal.com>
2025-03-09 15:52:57 -0400

    implements + improve search results

--- src/views/TurkishWordnet.vue
+++ src/views/TurkishWordnet.vue
@@ -1,114 +1,112 @@
 <script setup>
 import SearchForm from "@/components/SearchForm.vue"
 import SearchResultsCounter from "@/components/SearchResultsCounter.vue"
-import turkishFrameNet from "@/data/turkish/turkish-framenet.json"
 import turkishWordNet from "@/data/turkish/turkish-wordnet.json"
-import turkishWordNet from "@/data/turkish/turkish-wordnet-0.json"
-import turkishWordNet from "@/data/turkish/turkish-wordnet-1.json"
-import turkishWordNet from "@/data/turkish/turkish-wordnet-2.json"
-import turkishWordNet from "@/data/turkish/turkish-wordnet-3.json"
-import turkishWordNet from "@/data/turkish/turkish-wordnet-4.json"
-import turkishWordNet from "@/data/turkish/turkish-wordnet-5.json"
-import turkishWordNet from "@/data/turkish/turkish-wordnet-6.json"
-import turkishWordNet from "@/data/turkish/turkish-wordnet-7.json"
-import turkishWordNet from "@/data/turkish/turkish-wordnet-8.json"
-import turkishWordNet from "@/data/turkish/turkish-wordnet-9.json"
+import turkishWordNet1901 from "@/data/turkish/turkish-wordnet-1901.json"
+import turkishWordNet1944 from "@/data/turkish/turkish-wordnet-1944.json"
+import turkishWordNet1955 from "@/data/turkish/turkish-wordnet-1955.json"
+import turkishWordNet1959 from "@/data/turkish/turkish-wordnet-1959.json"
+import turkishWordNet1966 from "@/data/turkish/turkish-wordnet-1966.json"
+import turkishWordNet1969 from "@/data/turkish/turkish-wordnet-1969.json"
+import turkishWordNet1974 from "@/data/turkish/turkish-wordnet-1974.json"
+import turkishWordNet1983 from "@/data/turkish/turkish-wordnet-1983.json"
+import turkishWordNet1988 from "@/data/turkish/turkish-wordnet-1988.json"
+import turkishWordNet1998 from "@/data/turkish/turkish-wordnet-1998.json"
 import { reactive, ref } from "vue"
 import * as utils from "@/utils/nlp"
 import { scrollToTop } from "@/utils/scrollToTop"
 
 defineOptions({ meta: { language: "Turkish", toolkit: "Wordnet" } })
 
+const turkishWordNets = {
+  1901: turkishWordNet1901,
+  1944: turkishWordNet1944,
+  1955: turkishWordNet1955,
+  1959: turkishWordNet1959,
+  1966: turkishWordNet1966,
+  1969: turkishWordNet1969,
+  1974: turkishWordNet1974,
+  1983: turkishWordNet1983,
+  1988: turkishWordNet1988,
+  1998: turkishWordNet1998,
+  2020: turkishWordNet
+}
+
+const years = [2020, 1998, 1988, 1983, 1974, 1969, 1966, 1959, 1955, 1944, 1901]
+
 const searchTerms = reactive({
   word: "", synonym: "", id: ""
 })
 const search = ref("")
-const searchResults = ref([])
+const searchResults = ref({})
 
-// function findFrame(frame) {
-//     search.value = frame
-//     searchTerms.frame = ""
-//     const entry = turkishFrameNet.find(entry => entry.frame == frame)
-//     searchResults.value = [{
-//         frame,
-//         lexicalUnits: entry?.lexicalUnits,
-//         frameElements: entry?.frameElements
-//     }]
-//     scrollToTop()
-// }
-// function findVerb(word) {
-//     search.value = word
-//     searchTerms.verb = ""
-//     searchResults.value = utils.getFramesForSynSets(utils.getSynsetsWithWord(word, turkishWordNet), turkishFrameNet)
-//     scrollToTop()
-// }
-// function findSynSetID(id) {
-//     search.value = id
-//     searchTerms.id = ""
-//     searchResults.value = utils.getFramesForSynSet(id, turkishFrameNet)
-//     scrollToTop()
-// }
+function findWord(word) {
+  search.value = word
+  searchTerms.word = ""
+  Object.entries(turkishWordNets).forEach(([year]) => {
+    searchResults.value[year] = utils.getSynsetsWithWord(search.value, turkishWordNets[year])
+  })
+  scrollToTop()
+}
+function findSynonym(synonym) {
+  search.value = synonym
+  searchTerms.synonym = ""
+  Object.entries(turkishWordNets).forEach(([year]) => {
+    searchResults.value[year] = utils.getSynsetsWithWord(search.value, turkishWordNets[year])
+  })
+  scrollToTop()
+}
+function findSynSetID(id) {
+  search.value = id
+  searchTerms.id = ""
+  Object.entries(turkishWordNets).forEach(([year]) => {
+    searchResults.value[year] = [utils.searchInWordnet(search.value, turkishWordNets[year])]
+  })
+  scrollToTop()
+}
 
 </script>
 
 <template>
-    <header class="uk-flex uk-flex-around uk-background-default" uk-sticky uk-grid>
-        <SearchForm @submit.prevent="findFrame(searchTerms.word)" v-model="searchTerms.word">Word</SearchForm>
-        <SearchForm @submit.prevent="findVerb(searchTerms.synonym)" v-model="searchTerms.synonym">Synonym</SearchForm>
-        <SearchForm @submit.prevent="findSynSetID(searchTerms.id)" v-model="searchTerms.id">SynSet Id</SearchForm>
-    </header>
-    <template v-if="search">
-        <SearchResultsCounter :searchResults="searchResults.length" :for="search" />
-    </template>
-    <ul uk-accordion>
-        <li v-for="(result, index) in searchResults" :key="index">
-            <a class="uk-accordion-title" href>{{ index + 1 }}. {{ result.frame }}</a>
-            <div class="uk-accordion-content">
-                <h2 class="uk-h3 uk-text-muted">
-                    Frame Elements
-                    <hr />
-                </h2>
-                <div uk-grid>
-                    <div class="uk-width-2-3">
-                        <span class="uk-label uk-margin-xsmall-right" v-for="frame in result.frameElements"
-                            :key="frame">
-                            {{ frame }}
-                        </span>
-                    </div>
-                    <div class="uk-width-1-3 uk-divider-vertical">
-                        <button class="uk-button uk-button-secondary uk-width-1-1 uk-height-1-1"
-                            @click="findFrame(result.frame)">
-                            Look up {{ result.frame }}
-                        </button>
-                    </div>
-                </div>
-                <h2 class="uk-h3 uk-text-muted uk-margin-small">
-                    Lexical Units
-                    <hr />
-                </h2>
-                <table class="uk-table uk-table-striped uk-table-hover uk-table-small">
-                    <thead>
-                        <tr>
-                            <th class="uk-width-1-5">Id</th>
-                            <th class="uk-width-2-5">Words</th>
-                            <th class="uk-width-2-5">Definition</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="unit in result.lexicalUnits" :key="unit">
-                            <td> <a @click="findSynSetID(unit)"> {{ unit }} </a> </td>
-                            <td>
-                                <template v-for="(word, index) in utils.searchInWordnet(unit, turkishWordNet)['words']"
-                                    :key="word">
-                                    <span v-if="index > 0">; </span>
-                                    <a @click="findVerb(word)">{{ word }}</a>
-                                </template>
-                            </td>
-                            <td> {{ utils.searchInWordnet(unit, turkishWordNet)["definition"] }} </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </li>
+  <header class="uk-flex uk-flex-around uk-background-default" uk-sticky uk-grid>
+    <SearchForm @submit.prevent="findWord(searchTerms.word)" v-model="searchTerms.word">Word</SearchForm>
+    <SearchForm @submit.prevent="findSynonym(searchTerms.synonym)" v-model="searchTerms.synonym">Synonym</SearchForm>
+    <SearchForm @submit.prevent="findSynSetID(searchTerms.id)" v-model="searchTerms.id">SynSet Id</SearchForm>
+  </header>
+  <template v-if="search">
+    <ul class="uk-child-width-expand" uk-tab>
+      <li v-for="year in years" :key="year">
+        <a href>{{ year }}</a>
+      </li>
     </ul>
+    <div class="uk-switcher">
+      <div v-for="year in years" :key="year">
+        <SearchResultsCounter :searchResults="searchResults[year].length" :for="search" />
+        <table class="uk-table uk-table-striped uk-table-hover uk-table-small" v-if="searchResults[year].length">
+          <thead>
+            <tr>
+              <th class="uk-width-1-5">Id</th>
+              <th class="uk-width-1-5">Pos</th>
+              <th class="uk-width-2-5">Definition</th>
+              <th class="uk-width-1-5">Synonyms</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="result in searchResults[year]" :key="result.id">
+              <td> <a @click="findSynSetID(result.id)"> {{ result.id }} </a> </td>
+              <td> {{ result.pos }} </td>
+              <td> {{ result.definition }} </td>
+              <td>
+                <template v-for="(word, index) in result.words.filter(word => word != search)" :key="word">
+                  <span v-if="index > 0">; </span>
+                  <a @click="findSynonym(word)">{{ word }}</a>
+                </template>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+    </div>
+  </template>
 </template>
