fb537904cc0dc463fcdd0efc64b47c5e6198ebb1
Kumsal Obuz <iam@denizkumsal.com>
2025-03-15 14:34:57 -0400

    removes unnecessary elements

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
   <NavBar />
   <main class="main uk-container" uk-height-viewport="expand: true">
-    <!-- <iframe src="" class="uk-width-1-1" /> -->
     <RouterView />
   </main>
 </template>
