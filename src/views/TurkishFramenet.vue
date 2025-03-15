dfd9a00bce321e30deea4ea9c3d170b5720acb82
Kumsal Obuz <iam@denizkumsal.com>
2025-03-15 14:34:47 -0400

    accounts for 0 results

--- src/views/TurkishFramenet.vue
+++ src/views/TurkishFramenet.vue
@@ -19,11 +19,14 @@ function findFrame(frame) {
   search.value = frame
   searchTerms.frame = ""
   const entry = turkishFrameNet.find(entry => entry.frame == frame)
+  if(entry){
   searchResults.value = [{
     frame,
     lexicalUnits: entry?.lexicalUnits,
     frameElements: entry?.frameElements
-  }]
+    }]
+  }
+  else searchResults.value = []
   scrollToTop()
 }
 function findVerb(word) {
